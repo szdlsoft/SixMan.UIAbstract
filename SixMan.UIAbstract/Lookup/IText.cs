d12f56041350af02e2a856e82393c844e2fe8f39
szdlsoft <szdlsoft@126.com>
2018-05-18 10:48:48 +0800

    加IText

---
+++ SixMan.UIAbstract/Lookup/IText.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixMan.UIAbstract.Lookup
+{
+    public interface IText
+    {
+        string Text { get; }
+    }
+}
