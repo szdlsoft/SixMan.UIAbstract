0eff3fa66bb0e50f4b26431bbdae11fac8cdaf2c
szdlsoft <szdlsoft@126.com>
2018-05-18 10:14:02 +0800

    添加项目文件。

---
+++ SixMan.UIAbstract/Lookup/Dto/LookUpItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixMan.UIAbstract.Lookup
+{
+    /// <summary>
+    /// 查找项目
+    /// </summary>
+    public class LookUpItem
+    {
+
+        public LookUpItem(long id, string displayName)
+        {
+            Id = id;
+            Text = displayName;
+        }
+
+        /// <summary>
+        /// Value: id
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// Text field
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ SixMan.UIAbstract/Lookup/Dto/LookUpSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixMan.UIAbstract.Lookup
+{
+    /// <summary>
+    /// 字典表
+    /// </summary>
+    public class LookUpSource
+        : List<LookUpItem>
+    {
+    }
+
+}
