b97c0943243489fc92126df156f477607054ed71
szdlsoft <szdlsoft@126.com>
2018-05-24 14:23:12 +0800

    加入 crud依赖的function

--- js/abpEasyuiSimpleCrud.js
+++ js/abpEasyuiSimpleCrud.js
@@ -12,6 +12,8 @@
 //    editForm: $("#fm")
 //});
 function AbpEasyuiSimpleCrud(option) {
+    initCrudCommon();
+
     this.currentEntity = {};
     $.extend(true, this, option);
 }
@@ -219,7 +221,73 @@ function imgFormater(imgFile) {
     return "<img style='width:24px;height:24px;' border='1' src='" + imgFile + "'/>";
 }
 
-function initForm() {
+function initCrudCommon() {
+    if ( Date.prototype.Format ) {
+        return; // 不重复
+    }
+
+    // 对Date的扩展，将 Date 转化为指定格式的String
+    // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+    // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+    // 例子：
+    // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+    // (new Date()).Format("yyyy-M-d h:m:s.S")   ==> 2006-7-2 8:9:4.18
+    Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+
+    //serializeFormToObject plugin for jQuery
+    $.fn.serializeFormToObject = function () {
+        //serialize to array
+        var data = $(this).serializeArray();
+        //add also disabled items
+        $(':disabled[name]', this).each(function () {
+            data.push({ name: this.name, value: $(this).val() });
+        });
+
+        //map to object
+        var obj = {};
+        data.map(function (x) { obj[x.name] = mergeSameKey(obj, x.name, x.value); });
+
+        return obj;
+    };
+
+    initFormDatebox();
+}
+
+//相同key 转数组
+function mergeSameKey(obj, name, value) {
+    var item = obj[name];
+    if (item) {
+        if (item instanceof Array) {
+        }
+        else {
+            var lastValue = item;
+            item = [];
+            item.push(lastValue);
+        }
+        item.push(value);
+        return item;
+    }
+    else {
+        return value;
+    }
+}
+
+
+function initFormDatebox() {
     $('.easyui-datebox').datebox({
         formatter: function (date) {
             if (date) {
@@ -238,7 +306,12 @@ function initForm() {
     });
 }
 
-
+function dateFormater(value) {
+    if (value) {
+        return new Date(value).Format("yyyy-MM-dd");
+    }
+    return value;
+}
 
 
 
